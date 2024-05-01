44af1f5860fe5ff4111279ec55b1158f7934f627
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-05-01 19:55:57 +0900

    React- hook - useEffect

---
+++ use-effect/src/App.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(1);
+  const [name, setName] = useState('');
+
+  const handleCountUpdate = () => {
+      setCount(count + 1);
+  };
+
+  // 마운팅
+  useEffect(() => {
+    console.log('마운팅');
+  }, []);
+
+  // 마운트 + [ name ] 변경될때만 실행
+  useEffect(() => {
+    console.log("렌더링");
+  });
+
+  // 마운트 + [ name ] 변경될때만 실행
+  useEffect(() => {
+    console.log("name 변화");
+  }, [name]);
+
+  // 마운트 + [ count ] 변경될때만 실행
+  useEffect(() => {
+    console.log("count 변화");
+  }, [count]);
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleCountUpdate}>Update</button>
+      <span>count: {count}</span>
+      <input type="text" value={name} onChange={handleInputChange}/>
+  <span>name {name}</span>
+    </div>
+  );
+}
+
+export default App;
