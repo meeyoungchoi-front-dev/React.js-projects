36a7ed1ff22a4c958ff4aafb917686365f8b7acd
choimeeyoung <choimeeyoung94@gmail.com>
2024-07-30 09:41:42 +0900

    React 사용하여 팝업창 열고 닫기 기능 구현

--- react-modal-popup/src/components/ModalPopup.js
+++ react-modal-popup/src/components/ModalPopup.js
@@ -1,30 +1,43 @@
-import React , { useRef, useEffect}  from 'react';
+import React , { useRef, useEffect, useState}  from 'react';
 import styles from '../App.css';
 
 const ModalPopup = () =>  {
- const toggleRef = useRef(null);
+const toggleRef = useRef(null);
+const [isPopupVisible, setPopupVisible] = useState(false);
+const [isActive, setIsActive] = useState(false);
 
- useEffect(() => {
-   const body = document.querySelector('body');
-   const toggle = toggleRef.current; 
-   if (toggle) {
-     toggle.onclick = function () {
-       toggle.classList.toggle('active');
-       body.classList.toggle('active');
-     };
-   }
- }, []);
-
-  return (
-    <div className="{styles.container}">
-        <button className="{styles.show-btn}">Show Popup</button>
-        <div className="{styles.spop-box}">
-            <h1>Hello World</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, dolores modi quaerat sapiente recusandae rerum veniam dolorem officiis! Hic voluptatem doloribus dolorum facere quo voluptatibus adipisci, quas alias sequi sit.</p>
-            <button className="{styles.close-pop}">OK</button>
-        </div>    
-    </div>
-  )
+const showPopup = () => {
+    setPopupVisible(true);
 }
+
+const closePopup = () => {
+    setPopupVisible(false);
+}
+ 
+return (
+    <div className={styles.container}>
+    {/* 팝업 열기 버튼 */}
+    <button className={`${styles.showBtn} ${isActive ? styles.active : ''}`} onClick={showPopup}>
+        Show Popup
+    </button>
+
+    {/* 팝업 조건부 렌더링 */}
+    {isPopupVisible && (
+        <div className={`${styles.popBox} ${isActive ? styles.active : ''}`}>
+        <h1>Hello World</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, dolores
+            modi quaerat sapiente recusandae rerum veniam dolorem officiis! Hic
+            voluptatem doloribus dolorum facere quo voluptatibus adipisci, quas
+            alias sequi sit.
+        </p>
+        {/* 팝업 닫기 버튼 */}
+        <button className={`${styles.closePop}`} onClick={closePopup}>
+            OK
+        </button>
+        </div>
+    )}
+</div>
+)}
 
 export default ModalPopup
